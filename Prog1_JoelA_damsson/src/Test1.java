850111d5a7ee51581c94cd5a8be587fa3b6f55f2
joeada1 <joeada1@5CD821551H.thn.local>
2019-09-11 14:53:48 +0200

    Hello world

---
+++ Prog1_JoelA_damsson/src/Test1.java
@@ -0,0 +1,22 @@
+
+public class Test1 {
+	public static void main(String [] arg){
+
+	int tal1; // deklarering	
+	tal1=1; // tilldelning
+	
+	int tal2=10; // deklarering + tilldelning = initiering 
+		
+	double d = 3.14;
+	
+	double d1=tal2;
+	System.out.println(d1);
+	
+	int tal3=(int) (20.66666+0.5);
+	System.out.println(tal1);
+	
+	
+	
+	
+	}
+}
